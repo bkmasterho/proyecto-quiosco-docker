4bf533a6a5a7c0505406622f3bb921facd8f559a
Jesus <bkmasterho.125@gmail.com>
2025-11-16 23:44:27 -0300

    cambiesllos 3

--- react-quiosco/src/context/QuioscoProvider.jsx
+++ react-quiosco/src/context/QuioscoProvider.jsx
@@ -6,19 +6,26 @@ const QuioscoContext = createContext();
 const QuioscoProvider = ({children}) => {
 
     const [categorias, setCategorias] = useState(categoriasDB);
-    const [categoriaActual, setCategoriaActual] = useState(categorias[0])    
+    const [categoriaActual, setCategoriaActual] = useState(categorias[0])  
+    const [modal, setModal] = useState(false)  
 
     const handleClickCategoria = id => {
         const categoria = categorias.filter(categoria => categoria.id === id)[0];
         setCategoriaActual(categoria)
     } 
 
+    const handleClickModal = () => {
+        setModal(!modal)
+    }
+
     return (
         <QuioscoContext.Provider
             value={{
                 categorias,
                 categoriaActual,
-                handleClickCategoria
+                handleClickCategoria,
+                modal,
+                handleClickModal
             }}
         >{children}</QuioscoContext.Provider>
     )
--- react-quiosco/src/layouts/Layout.jsx
+++ react-quiosco/src/layouts/Layout.jsx
@@ -1,9 +1,29 @@
 import { Outlet } from "react-router-dom"
+import Modal from 'react-modal'
 import Sidebar from "../components/Sidebar"
 import Resumen from "../components/Resumen"
+import useQuiosco from "../hooks/useQuiosco"
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 export default function Layout() {   
+
+  const { modal } = useQuiosco();
+
+  console.log("Elmodal",modal)
+
   return (
+
+    <>
       <div className="md:flex">
 
         <Sidebar/>
@@ -15,5 +35,6 @@ export default function Layout() {
         <Resumen/>
 
       </div>
+    </>
   )
 }
